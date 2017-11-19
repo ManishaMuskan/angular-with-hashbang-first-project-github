d4e257747b0776559f5aecdb194ff749a1df4a81
Manisha <composetomani@gmail.com>
2017-11-19 19:29:17 +0530

    Initial Commit

---
+++ Scripts/Controllers.js
@@ -0,0 +1,61 @@
+﻿app.controller("page1Controller", function ($scope) {
+    $scope.title = "Page 1 title";
+    })
+    .controller("page2Controller", function ($scope) {
+        $scope.title = "Page 2 title ";
+    })
+    .controller("myController", function ($scope, $routeParams) {
+        $scope.message = "Hello World";
+        var learner = [{
+            id:1,
+            name: "Dibyanshu",
+            gender: "Male"
+        }, {
+            id: 2,
+            name: "Manisha",
+            gender: "Female"
+        }, {
+            id: 3,
+            name: "Anuradha",
+            gender: "Female"
+        }, {
+            id: 4,
+            name: "Sunny",
+            gender: "Male"
+        }, {
+            id: 5,
+            name: "Shanu",
+            gender: "Male"
+        }, {
+            id: 6,
+            name: "Gautam",
+            gender: "Male"
+        }, {
+            id: 7,
+            name: "Shefali",
+            gender: "Female"
+        }, {
+            id: 8,
+            name: "Deepti",
+            gender: "Female"
+        }];
+        $scope.showDetails = false;
+        $scope.learner = learner;
+        $scope.l_id = $routeParams.id - 1;
+        $scope.details = { id: 1, name: "Mani", gender: "Female" };
+        $scope.mani = function (item) {
+            $scope.showDetails = true;
+            $scope.student = item;
+        };
+
+        //$scope.details = { id: learner[l_id].id, name: learner[l_id].name, gender: learner[l_id].gender }
+    })
+    .controller("DetailsController", function ($scope) {
+        $scope.details = {id : 1, name:"Mani", gender:"Female"};
+    })
+    .controller("ApiDataController", function ($scope, $http) {
+        $http.get("https://jsonplaceholder.typicode.com/posts/1")
+            .then(function (response) {
+                $scope.responseData = response.data;
+            });
+    });
---
+++ Scripts/Script.js
@@ -0,0 +1,40 @@
+﻿/// <reference path="angular.js" />
+/// <reference path="angular-route.min.js" />
+
+var app = angular.module("Demo", ["ngRoute"])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider.
+        when("/", {
+            templateUrl: "Views/page1.html",
+            controller: "page1Controller"
+        }).
+        when("/page1", {
+            templateUrl: "Views/page1.html",
+            controller: "page1Controller"
+        }).
+        when("/page2", {
+            templateUrl: "Views/page2.html",
+            controller: "page2Controller"
+        }).
+        when("/ApiData", {
+            templateUrl: "Views/ApiData.html",
+            controller: "ApiDataController"
+        }).
+        when("/Template", {
+            templateUrl: "Views/Template.html",
+            controller: "myController"
+        }).
+        when("/backToList", {
+            templateUrl: "Views/Template.html",
+            controller: "myController"
+        }).
+        when("/learner/:id", {
+            templateUrl: "Views/Learnerdetails.html",
+            controller: "myController"
+        }).
+        otherwise({
+            redirectTo: "/"
+        });
+        //$locationProvider.html5Mode(true).hashPrefix('!');
+        // $locationProvider.hashPrefix('!');
+    });
